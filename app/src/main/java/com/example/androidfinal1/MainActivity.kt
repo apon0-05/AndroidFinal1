a6cc793086ea53a2ff1ea9f46a4924f2542d4667
Danial Issakov <danialissakov@gmail.com>
2024-12-01 14:08:20 +0500

    profilepage

--- app/src/main/java/com/example/androidfinal1/MainActivity.kt
+++ app/src/main/java/com/example/androidfinal1/MainActivity.kt
@@ -6,16 +6,53 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.androidfinal1.store.presentation.components.ItemView
 import com.example.androidfinal1.store.presentation.screen.ActorPage
 import com.example.androidfinal1.store.presentation.navigation.BottomNavigationBar
 import com.example.androidfinal1.store.presentation.screen.CategoryScreen
@@ -26,6 +63,7 @@ import com.example.androidfinal1.store.presentation.screen.Gallery
 import com.example.androidfinal1.store.presentation.screen.NewHomePage
 import com.example.androidfinal1.store.presentation.navigation.Screen
 import com.example.androidfinal1.store.presentation.viewmodel.MoviesViewModel
+import com.example.androidfinal1.store.presentation.viewmodel.ScreenState
 import com.example.androidfinal1.ui.theme.AndroidFinal1Theme
 
 class MainActivity : ComponentActivity() {
@@ -97,6 +135,5 @@ fun SearchPage(navController: NavController){
 }
 
 @Composable
-fun ProfilePage(navController: NavController){
-
+fun ProfilePage(navController: NavController) {
 }
