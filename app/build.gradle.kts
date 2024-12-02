660d9fa8b285873738779df288ea2b3f4b6320c9
Zhanarym Elemes <zhanarymelemes@MacBook-Pro-Zhanarym.local>
2024-12-02 15:39:55 +0500

    search page ui and some functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,4 +95,6 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.5.1")
     implementation ("androidx.navigation:navigation-compose:2.7.3")
     implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation ("androidx.compose.material3:material3:1.1.1")
+
 }
